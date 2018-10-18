9691dfa0edecf1d930dc1775bb35f54c0b90c02c
Ethan Casper <32209260+NukeBurner@users.noreply.github.com>
2018-10-18 16:46:34 -0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('../sys/config.js')
+module.exports = {
+    name: 'ping',
+    description: 'Ping!',
+    execute(message, args) {
+         const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    },
+};
