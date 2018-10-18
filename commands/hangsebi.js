dfbe7bc01e5515408ca70662ac6e80cca16c8e58
Ethan Casper <32209260+NukeBurner@users.noreply.github.com>
2018-10-18 16:44:03 -0700

    Create hangsebi.js

---
+++ commands/hangsebi.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+var usage = "`!! hangsebi <channelid> <your phrase>`\n`Example: !! hangsebi grandest nan is gay`";
+var stages = [`\`\`\`
+/---|
+|   
+|
+|
+|
+\`\`\`
+`, `\`\`\`
+/---|
+|   o
+|
+|
+|
+\`\`\`
+`, `\`\`\`
+/---|
+|   o
+|   |
+| 
+|
+\`\`\`
+`, `\`\`\`
+/---|
+|   o
+|  /|
+|
+|
+\`\`\`
+`, `\`\`\`
+/---|
+|   o
+|  /|\\
+|
+|
+\`\`\`
+`, `\`\`\`
+/---|
+|   o
+|  /|\\
+|  /
+|
+\`\`\`
+`, `\`\`\`
+/---|
